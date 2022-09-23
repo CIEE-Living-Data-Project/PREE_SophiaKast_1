dc44e49021f18ccad5295b9eba111bf6b51abc5f
Sophia Kast <sophia.kast@gmail.com>
2022-09-23 13:40:44 -0700

    Data cleanup

--- scripts/Data_Load_Cleanup.R
+++ scripts/Data_Load_Cleanup.R
@@ -9,13 +9,17 @@ groundhog.library('tidyverse', '2022-09-01')
 #' `readr` is a package within `tidyverse` that allows loading of data without downloading data.
 #' load .csv data using `readr`, which pulls the data in straight from the url
 #' Assign object name as 'salmon'
-salmon <- readr::read_csv('https://whitlockschluter3e.zoology.ubc.ca/Data/chapter12/chap12e4ChinookWithBrookTrout.csv')
+salmon_raw <- readr::read_csv('https://whitlockschluter3e.zoology.ubc.ca/Data/chapter12/chap12e4ChinookWithBrookTrout.csv')
 
 #' load `skimr`, a package that summarizes data
 #' function is `skim()` to get data summary
 groundhog.library('skimr', '2022-09-01')
-skim(salmon)
+skim(salmon_raw)
 
-class(salmon)
-salmon
+class(salmon_raw)
+salmon_raw
 
+#' use `%>%` to string multiple functions together into one object. 
+#' `%^%` goes between each complete function.
+salmon <- %>% 
+  
