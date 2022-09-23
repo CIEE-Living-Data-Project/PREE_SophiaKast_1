4e7b2fcd061f5f7bae30fe31ed9536934bca7ef9
Sophia Kast <sophia.kast@gmail.com>
2022-09-23 16:17:22 -0700

    updating clean up script, learning things

--- scripts/Data_Load_Cleanup.R
+++ scripts/Data_Load_Cleanup.R
@@ -4,7 +4,7 @@ library(groundhog)
 
 #' Use groundhog to load and install packages with date for version control.
 #' load tidyverse
-groundhog.library('tidyverse', '2022-09-01')
+groundhog.library(c('tidyverse', 'skimr'), '2022-09-01')
 
 #' `readr` is a package within `tidyverse` that allows loading of data without downloading data.
 #' load .csv data using `readr`, which pulls the data in straight from the url
@@ -21,5 +21,8 @@ salmon_raw
 
 #' use `%>%` to string multiple functions together into one object. 
 #' `%^%` goes between each complete function.
-salmon <- %>% 
+salmon
   
+# write a new file: write.csv()
+
+plot(salmon_raw$nSurvivors~salmon_raw$nReleased)
